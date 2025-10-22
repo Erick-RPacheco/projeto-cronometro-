d140e0a22007a78e6bf12216ef94a3f444be5fe3
Erick-RPacheco <erick84233248@gmail.com>
2025-10-22 01:59:03 -0300

    adição de tema escuro

--- script.js
+++ script.js
@@ -2,10 +2,30 @@ const display = document.getElementById("display");
 const botaoStart = document.getElementById("start");
 const botaoPause = document.getElementById("pause");
 const botaoReset = document.getElementById("reset");
-
+const botaoTema = document.getElementById("tema");
 
 let segundos = 0;
 let timer = null;
+let tema = "claro";
+
+function trocarTema() {
+  if (tema === "claro") {
+    document.body.style.background = "#2e2e2e";
+    display.style.color = "#d3d3d4"
+    botaoTema.style.background = "#d3d3d4"
+    tema = "escuro";
+    return;
+  }
+
+  if (tema === "escuro") {
+    document.body.style.background = "#f5f5f5";
+    display.style.color = "black"
+    botaoTema.style.background = "#2e2e2e"
+    tema = "claro";
+   }
+}
+
+botaoTema.addEventListener("click", trocarTema);
 
 function atualizarDisplay() {
   const horas = Math.floor(segundos / 3600);
